07138cf29df3402e74db5dbcd4abbeb82d2d543f
Zachary <75510412+Zachary1575@users.noreply.github.com>
2022-10-17 14:45:08 -0400

    Changed to safe area view
    Changed to safe area view

--- WhatWorksClient/src/screens/Main.tsx
+++ WhatWorksClient/src/screens/Main.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Text, Button } from 'react-native';
 
 export default function Main({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "white" }}>
+  <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "white" }}>
     <Text style={styles.Title}>Welcome to WhatWorks App Directory!</Text>
     <Button title="Cape Cod Potato Chips"
     onPress={() => navigation.navigate('Review1')}/>
     <Button title="Cape Cod Potato Chips Part 2"
     onPress={() => navigation.navigate('Review2')}/>
-  </View>
+  </SafeAreaView>
 );
 
 }
