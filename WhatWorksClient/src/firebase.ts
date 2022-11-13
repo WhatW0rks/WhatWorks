abe818904e7bd405530193e4ae3d0a1d3a461f78
tostig77 <67396590+tostig77@users.noreply.github.com>
2022-11-13 15:56:29 -0500

    updating firebase.ts

--- WhatWorksClient/src/firebase.ts
+++ WhatWorksClient/src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import environment from "./environment";
+import environment from "../../WhatWorks/automation/environment";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
